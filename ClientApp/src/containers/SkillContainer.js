51b13d98f6af2d315fb3909cb4a5b670c78dc94b
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2019-01-19 15:38:55 -0800

    Update SkillContainer.js
    Changed the information SkillContainer was passing and passed it to AbilityScoreBox.

--- ClientApp/src/containers/SkillContainer.js
+++ ClientApp/src/containers/SkillContainer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Abs } from '../components/Abs';
+import {AbilityScoreBox} from '../components/AbilityScoreBox';
 
-const Skills = [{name: 'Strength', score: 9, skills: [{name: 'Saving Throw', proficient: false}, {name: 'Athletics', proficient:true}] },
+const proficiencyBonus = 3;
+const AbilityScoreInfo = [{name: 'Strength', score: 9, skills: [{name: 'Saving Throw', proficient: false}, {name: 'Athletics', proficient:true}] },
                         {name: 'Dexterity', score: 16, skills: [{name: 'Saving Throw', proficient: false}, {name: 'Acrobatics', proficient:true}, {name: 'Sleight of Hand', proficient:false}, {name: 'Stealth', proficient:false}] },
                         {name: 'Constitution', score: 13, skills: [{name: 'Saving Throw', proficient: false}]},
                         {name: 'Inteligence', score: 12, skills: [{name: 'Saving Throw', proficient: true}, {name: 'Arcana', proficient:false}, {name: 'History', proficient:false}, {name: 'Investigation', proficient:false},  {name: 'Nature', proficient:false}, {name: 'Religion', proficient:false}] },
@@ -13,16 +14,20 @@ const Skills = [{name: 'Strength', score: 9, skills: [{name: 'Saving Throw', pro
 export class SkillContainer extends Component {
   
     constructor (props) {
-      super(props);
-
+        super(props);
+        this.RawToModifierConverter.bind(this);
+        this.state = {'AbilityScoreInfo':AbilityScoreInfo,'proficiencyBonus':proficiencyBonus}
     }
-  
+    RawToModifierConverter (score){
+        //Subtracts 10 from the raw score, divides by 2 rounding down, and returns a new value to be used as a modifer
+        return Math.floor((score-10)/2); 
+    }
+
     render () {
         return ( 
-        <div />
-        // <div>
-        //     {Skills.map((skill, i) => <Abs {...skill} key={i} />)}
-        // </div>
+        <div>
+            {this.state.AbilityScoreInfo.map((attribute, i) => <AbilityScoreBox {...attribute} proficiencyBonus={proficiencyBonus} rawToModifierConverter={this.RawToModifierConverter} key={i} />)}
+        </div>
         );
     }
 }
