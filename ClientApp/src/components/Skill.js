c8a2361b2688be3b0a364a0619614e6dc9b1feb6
Doctorbingbing <36774670+Doctorbingbing@users.noreply.github.com>
2019-01-19 15:38:16 -0800

    Create Skill.js
    Added a skill component that renders skill check modifiers. To be a subcomponent of AbilityScoreBox

---
+++ ClientApp/src/components/Skill.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class Skill extends Component{
+
+    render() {
+        return(
+            <div>
+            <label>
+                <input
+                    type="checkbox"
+                    value="Proficiency"
+                    onChange = {() => this.props.handleClick(this.props.name)}
+                ></input>
+                {this.props.name} {this.props.score}
+            </label>            
+            </div>
+
+        )
+    }
+}
